af86dd9223ee18083254592542eabe4e27682869
Calupator <calupator@mail.ru>
2023-01-03 07:42:03 +0300

    GB-DB - 03.01.2023 07:41:52

--- DZ-2.db.sql
+++ DZ-2.db.sql
@@ -1,76 +1,299 @@
-BEGIN TRANSACTION;
-DROP TABLE IF EXISTS "Люди";
-CREATE TABLE IF NOT EXISTS "Люди" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"ФИО"	TEXT,
-	"Д/р"	TEXT,
-	"Статус"	TEXT,
-	PRIMARY KEY("id" AUTOINCREMENT)
-);
-DROP TABLE IF EXISTS "Адреса";
-CREATE TABLE IF NOT EXISTS "Адреса" (
-	"Чей адрес"	INTEGER,
-	"Адрес"	TEXT,
-	"Комментарий"	TEXT
-);
-DROP TABLE IF EXISTS "Телефоны";
-CREATE TABLE IF NOT EXISTS "Телефоны" (
-	"Чей телефон"	INTEGER,
-	"Тел"	TEXT,
-	"Коммент"	TEXT
-);
-DROP TABLE IF EXISTS "Общий список";
-CREATE TABLE IF NOT EXISTS "Общий список" (
-	"ФИО"	TEXT,
-	"Д/р"	TEXT,
-	"Статус"	TEXT,
-	"Тел"	TEXT,
-	"Комментарий"	TEXT,
-	"Адрес"	TEXT,
-	"Группа"	TEXT
-);
-INSERT INTO "Люди" VALUES (1,'Иванов И. И.','12.02.1990','женат');
-INSERT INTO "Люди" VALUES (2,'Иванов И. И.','18.09.2001','холост');
-INSERT INTO "Люди" VALUES (3,'Петров П. П.','23.04.1983','женат');
-INSERT INTO "Люди" VALUES (4,'Васильев В. В.','21.05.1998','холост');
-INSERT INTO "Люди" VALUES (25,'Кузьмин К.К.','21.05.2020','холост');
-INSERT INTO "Адреса" VALUES (1,'Можга','Место рождения');
-INSERT INTO "Адреса" VALUES (1,'Казань','По прописке');
-INSERT INTO "Адреса" VALUES (1,'Москва','Рабочий');
-INSERT INTO "Адреса" VALUES (2,'Санкт-Петербург','По прописке');
-INSERT INTO "Адреса" VALUES (3,'Москва','По прописке');
-INSERT INTO "Адреса" VALUES (4,'Белгород','По прописке');
-INSERT INTO "Адреса" VALUES (5,'Уфа','По прописке');
-INSERT INTO "Адреса" VALUES (6,'Сочи','По прописке');
-INSERT INTO "Адреса" VALUES (7,'Киров','Рабочий');
-INSERT INTO "Адреса" VALUES (8,'Владивосток','Место рождения');
-INSERT INTO "Адреса" VALUES (9,'Рязань','Рабочий');
-INSERT INTO "Адреса" VALUES (10,'Хабаровск','Место рождения');
-INSERT INTO "Телефоны" VALUES (1,'123','личный');
-INSERT INTO "Телефоны" VALUES (1,'124','рабочий');
-INSERT INTO "Телефоны" VALUES (1,'125','для поездок');
-INSERT INTO "Телефоны" VALUES (1,'126','интернет');
-INSERT INTO "Телефоны" VALUES (1,'127','старый');
-INSERT INTO "Телефоны" VALUES (2,'527','личный');
-INSERT INTO "Телефоны" VALUES (3,'234','личный');
-INSERT INTO "Телефоны" VALUES (3,'235','рабочий');
-INSERT INTO "Телефоны" VALUES (4,'456','личный');
-INSERT INTO "Телефоны" VALUES (11,'999','личный');
-INSERT INTO "Телефоны" VALUES (12,'997','рабочий');
-INSERT INTO "Телефоны" VALUES (13,'995','личный');
-INSERT INTO "Телефоны" VALUES (14,'993','для поездок');
-INSERT INTO "Телефоны" VALUES (15,'991','для поездок');
-INSERT INTO "Общий список" VALUES ('Иванов И. И.','12.02.1990','женат','126','интернет','Можга','Работа');
-INSERT INTO "Общий список" VALUES ('Иванов И. И.','12.02.1990','женат','127','старый','Можга','Работа');
-INSERT INTO "Общий список" VALUES ('Иванов И. И.','18.09.2001','холост','527','личный','Санкт-Петербург','Школа');
-INSERT INTO "Общий список" VALUES ('Петров П. П.','23.04.1983','женат','234','личный','Москва','Родня');
-INSERT INTO "Общий список" VALUES ('Петров П. П.','24.04.1983','женат','235','рабочий','Москва','Родня');
-INSERT INTO "Общий список" VALUES ('Васильев В. В.','21.05.1998','холост','456','личный','Белгород','Друзья');
-INSERT INTO "Общий список" VALUES ('Сидоров С.С.','13.07.2007','холост','643','личный','Киров','Родня');
-INSERT INTO "Общий список" VALUES ('Сидоров С.С.','13.07.2007','холост','654','старый','Киров','Родня');
-INSERT INTO "Общий список" VALUES ('Александров А.А.','05.03.1987','женат','412','рабочий','Уфа','Друзья');
-INSERT INTO "Общий список" VALUES ('Борисов Б.Б','16.08.1989','холост','723','рабочий','Москва','Работа');
-INSERT INTO "Общий список" VALUES ('Михайлов М.М.','08.01.1997','женат','876','личный','Сочи','Университет');
-INSERT INTO "Общий список" VALUES ('Михайлов М.М.','08.01.1997','женат','875','рабочий','Сочи','Университет');
-INSERT INTO "Общий список" VALUES ('Кириллов К.К.','03.12.1996','холост','933','личный','Киров','Работа');
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Янв 03 2023 г., 07:37
+-- Версия сервера: 5.6.51
+-- Версия PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `GeekBrains`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `program`
+--
+
+CREATE TABLE `program` (
+  `id_prog` int(10) NOT NULL COMMENT 'Идентификатор образовательной программы'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Программа';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `type_event`
+--
+
+CREATE TABLE `type_event` (
+  `id_type_event` int(10) NOT NULL COMMENT 'Идентификатор типа мероприятия',
+  `type` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Тип мероприятия'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Тип мероприятия';
+
+--
+-- Дамп данных таблицы `type_event`
+--
+
+INSERT INTO `type_event` (`id_type_event`, `type`) VALUES
+(1, 'Хакатон'),
+(2, 'Конференция'),
+(3, 'Семинар'),
+(4, 'Лекция'),
+(5, 'Практикум');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `type_event_list`
+--
+
+CREATE TABLE `type_event_list` (
+  `id_event_list` int(10) NOT NULL COMMENT 'Идентификатор вида мероприятия',
+  `event_type` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Формат обучения'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Вид мероприятия';
+
+--
+-- Дамп данных таблицы `type_event_list`
+--
+
+INSERT INTO `type_event_list` (`id_event_list`, `event_type`) VALUES
+(1, 'Синхронный'),
+(2, 'Не синхронный');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `type_material`
+--
+
+CREATE TABLE `type_material` (
+  `id_material` int(10) NOT NULL,
+  `materials` text CHARACTER SET cp1251
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Тип материала';
+
+--
+-- Дамп данных таблицы `type_material`
+--
+
+INSERT INTO `type_material` (`id_material`, `materials`) VALUES
+(1, 'Видео'),
+(2, 'Аудио'),
+(3, 'Текстовые (книги, статьи)');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `type_task`
+--
+
+CREATE TABLE `type_task` (
+  `id_task` int(11) NOT NULL COMMENT 'Идентификатор задания',
+  `type` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Тип задания'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Тип задания';
+
+--
+-- Дамп данных таблицы `type_task`
+--
+
+INSERT INTO `type_task` (`id_task`, `type`) VALUES
+(1, 'Тест'),
+(2, 'Д/З');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(10) NOT NULL COMMENT 'Идентификатор пользователя',
+  `surname` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Фамилия',
+  `name` int(255) NOT NULL COMMENT 'Имя',
+  `patronymic` int(255) NOT NULL COMMENT 'Отчество',
+  `birthday` date NOT NULL COMMENT 'Дата рождения'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Данные пользователя';
+
+--
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id_user`, `surname`, `name`, `patronymic`, `birthday`) VALUES
+(1, '[value-2]', 0, 0, '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user_mail`
+--
+
+CREATE TABLE `user_mail` (
+  `id_mail` int(10) NOT NULL COMMENT 'Идентификатор E-mail',
+  `mail` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'E-mail пользователя'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='E-mail пользователя';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user_phone`
+--
+
+CREATE TABLE `user_phone` (
+  `id_user_phone` int(10) NOT NULL COMMENT 'Идентификатор телефона',
+  `phone` int(32) NOT NULL COMMENT 'Номер телефона пользователя'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Номер телефона пользователя';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user_status`
+--
+
+CREATE TABLE `user_status` (
+  `id_status` int(10) NOT NULL COMMENT 'Идентификатор типа/статуса пользователя',
+  `type` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Тип/статус пользователя'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Статус пользователя';
+
+--
+-- Дамп данных таблицы `user_status`
+--
+
+INSERT INTO `user_status` (`id_status`, `type`) VALUES
+(1, 'Ученик'),
+(2, 'Преподаватель'),
+(3, 'Наставник'),
+(4, 'Ревьюер'),
+(5, 'Администратор'),
+(6, 'Директор');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `program`
+--
+ALTER TABLE `program`
+  ADD PRIMARY KEY (`id_prog`);
+
+--
+-- Индексы таблицы `type_event`
+--
+ALTER TABLE `type_event`
+  ADD PRIMARY KEY (`id_type_event`);
+
+--
+-- Индексы таблицы `type_event_list`
+--
+ALTER TABLE `type_event_list`
+  ADD PRIMARY KEY (`id_event_list`);
+
+--
+-- Индексы таблицы `type_material`
+--
+ALTER TABLE `type_material`
+  ADD PRIMARY KEY (`id_material`);
+
+--
+-- Индексы таблицы `type_task`
+--
+ALTER TABLE `type_task`
+  ADD PRIMARY KEY (`id_task`);
+
+--
+-- Индексы таблицы `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Индексы таблицы `user_mail`
+--
+ALTER TABLE `user_mail`
+  ADD PRIMARY KEY (`id_mail`);
+
+--
+-- Индексы таблицы `user_phone`
+--
+ALTER TABLE `user_phone`
+  ADD PRIMARY KEY (`id_user_phone`);
+
+--
+-- Индексы таблицы `user_status`
+--
+ALTER TABLE `user_status`
+  ADD PRIMARY KEY (`id_status`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `program`
+--
+ALTER TABLE `program`
+  MODIFY `id_prog` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор образовательной программы';
+
+--
+-- AUTO_INCREMENT для таблицы `type_event`
+--
+ALTER TABLE `type_event`
+  MODIFY `id_type_event` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор типа мероприятия', AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `type_event_list`
+--
+ALTER TABLE `type_event_list`
+  MODIFY `id_event_list` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор вида мероприятия', AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `type_material`
+--
+ALTER TABLE `type_material`
+  MODIFY `id_material` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `type_task`
+--
+ALTER TABLE `type_task`
+  MODIFY `id_task` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор задания', AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор пользователя', AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `user_mail`
+--
+ALTER TABLE `user_mail`
+  MODIFY `id_mail` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор E-mail';
+
+--
+-- AUTO_INCREMENT для таблицы `user_phone`
+--
+ALTER TABLE `user_phone`
+  MODIFY `id_user_phone` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор телефона';
+
+--
+-- AUTO_INCREMENT для таблицы `user_status`
+--
+ALTER TABLE `user_status`
+  MODIFY `id_status` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор типа/статуса пользователя', AUTO_INCREMENT=7;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
